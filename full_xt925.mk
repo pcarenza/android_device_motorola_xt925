f26a484863d0db382e28b5e271f0f94f115f0b2b
pcarenza <pcarenza@gmail.com>
2013-05-14 10:25:33 -0400

    changed to xt925 in file

--- full_xt925.mk
+++ full_xt925.mk
@@ -1,5 +1,5 @@
 $(call inherit-product, device/motorola/xt925/device_xt925.mk)
-PRODUCT_COPY_FILES +=device/motorola/xt926/apns-gsm-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES +=device/motorola/xt925/apns-gsm-conf.xml:system/etc/apns-conf.xml
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
