f1cec03603b0e349081e48e4ec6f4484d473f6e3
pcarenza <pcarenza@gmail.com>
2013-05-14 09:23:46 -0400

    added new apns to xt925

--- full_xt925.mk
+++ full_xt925.mk
@@ -1,6 +1,7 @@
+$(call inherit-product, device/motorola/xt925/device_xt925.mk)
+PRODUCT_COPY_FILES +=device/motorola/xt926/apns-gsm-conf.xml:system/etc/apns-conf.xml
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, device/motorola/xt925/device_xt925.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := xt925
